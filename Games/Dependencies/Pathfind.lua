f72fa6d33c9e1e5aaad2d10a0d5e178b625caa54
JinxTheCat <59938808+JinxTheCatto@users.noreply.github.com>
2023-08-03 02:51:21 -0700

    err

--- Games/Dependencies/Pathfind.lua
+++ Games/Dependencies/Pathfind.lua
@@ -299,11 +299,10 @@ function Path:Run(target)
 	if not pathComputed
 		or self._path.Status == Enum.PathStatus.NoPath
 		or #self._path:GetWaypoints() < 2
-		or (self._humanoid and self._humanoid:GetState() == Enum.HumanoidStateType.Freefall) then
+		--[[or (self._humanoid and self._humanoid:GetState() == Enum.HumanoidStateType.Freefall)]] then
 		self._visualWaypoints = destroyVisualWaypoints(self._visualWaypoints)
 		task.wait()
 		declareError(self, self.ErrorType.ComputationError)
-		warn("COMP FAILED: "..err)
 		return false
 	end
 
